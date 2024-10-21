70c4a92f7c379147bbb37dc0284cb63402a28789
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-10-21 18:37:05 +0300

    update

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ import time
 if not os.path.exists('images'):
     os.mkdir('images')
 
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+base_url = "https://shikimori.one/animes/page/{}"
+image_counter = 0
+
+for page_number in range(1,10):
+    url = base_url.format(page_number)
+    page = requests.get(url, headers=header)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    images = soup.find_all('img')
+    for image in images:
+        image_url = image['src']
+        image_data = requests.get(image_url).content
+        with open('images/img{}.jpg'.format(image_counter), 'wb') as handler:
+            handler.write(image_data)
+        image_counter += 1
+        time.sleep(2)
+        print('Image {} скачивается'.format(image_counter))
+
 # page = requests.get(url, headers=headers)
 # soup = BeautifulSoup(page.text, 'html.parser')
 # print(soup.prettify())
@@ -22,18 +41,5 @@ if not os.path.exists('images'):
 #     with open('images/img{}.jpg'.format(i), 'wb') as handler:
 #         handler.write(img_data)
 
-def get_image(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    links = soup.find_all('img')
-    for i, url in enumerate(links):
-        time.sleep(2)
-        img_url = url['src']
-        img_data = requests.get(img_url).content
-        with open('images/img{}.jpg'.format(i), 'wb') as handler:
-            handler.write(img_data)
-
-url = 'https://shikimori.one/animes'
 
 
