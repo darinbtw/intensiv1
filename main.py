901427b837f08498e305d23a91058abaa5569032
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-10-22 14:19:09 +0300

    update parsing

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ moscow = cianparser.CianParser(location='Москва')
 # print(sorted_price)
 
 data = []
-for i in range(1, 400):
+for i in range(83, 100):
     a = moscow.get_flats(deal_type='sale', rooms=(1,2), additional_settings={'start_page': i, 'end_page':i} )
     data.extend(a)
-    time.sleep(20)
+    time.sleep(10)
 
 exel_export = pd.DataFrame(data)
 columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
 selected_columns = exel_export[columns]
-selected_columns.to_csv('parcing_from cian', mode='a', header=False, index=False)
+selected_columns.to_csv('cian_parsing.csv', mode='a', header=False, index=False)
 
